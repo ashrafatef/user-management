0c62ede8e47786ef0b578eaac4f46a10e12d8e64
ashafatef <ashraf.atef@swiftell.com>
2021-03-28 14:05:06 +0200

    update permission

--- db/migrations/000003_create_organizations_roles_table.up.sql
+++ db/migrations/000003_create_organizations_roles_table.up.sql
@@ -1,20 +1,20 @@
--- CREATE TABLE public.organization_roles
--- (
---     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
---     name character varying(50) COLLATE pg_catalog."default" NOT NULL,
---     description character varying(50) COLLATE pg_catalog."default",
---     organization_id integer NOT NULL,
---     created_at timestamp with time zone,
---     updated_at timestamp with time zone,
---     CONSTRAINT organization_roles_pkey PRIMARY KEY (id),
---     CONSTRAINT organization_id FOREIGN KEY (id)
---         REFERENCES public.organizations (id) MATCH SIMPLE
---         ON UPDATE CASCADE
---         ON DELETE CASCADE
---         NOT VALID
--- )
+CREATE TABLE public.organization_roles
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    description character varying(50) COLLATE pg_catalog."default",
+    organization_id integer NOT NULL,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    CONSTRAINT organization_roles_pkey PRIMARY KEY (id),
+    CONSTRAINT organization_id FOREIGN KEY (id)
+        REFERENCES public.organizations (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
 
--- TABLESPACE pg_default;
+TABLESPACE pg_default;
 
--- ALTER TABLE public.organization_roles
---     OWNER to botter;
+ALTER TABLE public.organization_roles
+    OWNER to botter;
